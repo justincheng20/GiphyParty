33b65d27fe55845e6ea521c0d0242bd04850648f
ngjamesng <just.james.ng@gmail.com>
2020-01-14 16:17:59 -0800

    add form submit, GIF URL fetching, and append/delete features

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
 $(function () {
 
-    console.log("Let's get this party started!");
-    const APIKey = "api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym";
-    async function getGif() {
-        let gif = await axios.get("http://api.giphy.com/v1/gifs/search?q=cats&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym");
+    async function getGif(searchTerm) {
+        const hostNameAndResource = "http://api.giphy.com/v1/gifs/search?q=";
+        const APIKey = "api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym";
+        // let gif = await axios.get("http://api.giphy.com/v1/gifs/search?q=cats&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym");
+        let gif = await axios.get(`${hostNameAndResource}${searchTerm}&${APIKey}`);
         let gifURL = gif.data.data[0].images.downsized_large.url;
-        console.log(gifURL);
+        return gifURL;
 
-        // let gifURL = 
     }
 
-    let gifsTable = $("#gifsTable");
-    gifsTable.append();
+    function appendToTable(gifURL){
+        const imageListItem = `<img class="gif" src=${gifURL}>`;
+        $("#gifList").append(imageListItem);
+    }
 
+    // let gifsTable = $("#gifsTable");
+    // gifsTable.append();
+    // Append to table after submit
+
+    $("#searchForm").on("submit", async function(e){
+        e.preventDefault();
+        let searchTerm = $("#formURL").val();
+        let gifURL = await getGif(searchTerm);
+        appendToTable(gifURL);
+        $(".gif").last().on("click", function(e){
+            //remove
+            e.target.remove();
+        })
+    });
+    
 })
